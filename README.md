9f2ab921f1092a3d71eb341715e0ecd8726991ca
echo-devim <devim@outlook.it>
2025-01-14 19:45:08 +0100

    Added extra info field

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 Nmap is a great open-source tool to perform a network inventory.
 This repo contains a simple script to convert the xml report produced by nmap into the more readable format xlsx (Excel).
+
+## Example Usage
+
+1. Run nmap:
+```sh
+$ sudo nmap -sV --script=http-title --system-dns -vv --script smb-os-discovery -O --scan-delay 100ms --max-scan-delay 300ms 10.20.30.0/24 20.30.40.0/24 -oX /tmp/scan.xml
+```
+
+2. Run this tool:
+```sh
+$ python3 nmapreport.py /tmp/scan.xml out.xlsx
+```
--- nmapreport.py
+++ nmapreport.py
@@ -1,6 +1,6 @@
 # NMAP Report Converter to Excel
 # Example command:
-# sudo nmap -sV --script=http-title --system-dns -vv -O --scan-delay 100ms --max-scan-delay 300ms 10.20.30.0/24 20.30.40.0/24 -oX /tmp/scan.xml
+# sudo nmap -sV --script=http-title --system-dns -vv --script smb-os-discovery -O --scan-delay 100ms --max-scan-delay 300ms 10.20.30.0/24 20.30.40.0/24 -oX /tmp/scan.xml
 
 import sys
 import xml.etree.ElementTree as ET
@@ -25,6 +25,7 @@ def parse_nmap_xml(xml_file):
         os = "n/a"
         hostname = "n/a"
         open_ports = []
+        extra = "n/a"
         
         if host.find('os'):
             os_info = host.find('os').find('osmatch')
@@ -37,6 +38,11 @@ def parse_nmap_xml(xml_file):
             hostname_info = host.find('hostnames').find('hostname')
             if hostname_info is not None:
                 hostname = hostname_info.get('name')
+        
+        if host.find('hostscript'):
+            script = host.find('hostscript').find('script')
+            if script is not None and script.get('id','') == "smb-os-discovery":
+                extra = script.get('output','n/a')
         
         for port in host.findall('.//port'):
             port_id = port.get('portid')
@@ -71,7 +77,8 @@ def parse_nmap_xml(xml_file):
             'mac_vendor': mac_vendor,
             'hostname': hostname,
             'service_count' : len(open_ports),
-            'open_ports': '\n'.join(open_ports)
+            'open_ports': '\n'.join(open_ports),
+            'extra' : extra
         })
     
     return scan_results
@@ -119,7 +126,7 @@ def create_excel(scan_results, output_file):
         #row += 1
         
         # Write the table headers
-        headers = ["Icon", "Host", "Operating System", "Hostname", "MAC Vendor", "Service count", "Open Ports"]
+        headers = ["Icon", "Host", "Operating System", "Hostname", "MAC Vendor", "Service count", "Open Ports", "Extra Info"]
         for header in headers:
             worksheet.write(row, col, header, header_format)
             col += 1
@@ -136,6 +143,7 @@ def create_excel(scan_results, output_file):
             worksheet.write(row, col + 4, host['mac_vendor'])
             worksheet.write(row, col + 5, host['service_count'])
             worksheet.write(row, col + 6, host['open_ports'])
+            worksheet.write(row, col + 7, host['extra'])
             row += 1
         
     # Autofit the worksheet.
